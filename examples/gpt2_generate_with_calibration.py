763c1b5ac3633faedef2038eec3e25b2f2881b6f
mikkyhu <myhu@princeton.edu>
2019-10-19 16:19:27 -0400

    calibration, monday report

--- examples/gpt2_generate_with_calibration.py
+++ examples/gpt2_generate_with_calibration.py
@@ -14,7 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+""" Conditional text generation with the auto-regressive models of the library (GPT/GPT-2/Transformer-XL/XLNet)
+"""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import argparse
@@ -25,10 +26,13 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
-from transformers import GPT2Config, OpenAIGPTConfig
+from transformers import GPT2Config, OpenAIGPTConfig, XLNetConfig, TransfoXLConfig
 
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from transformers import OpenAIGPTLMHeadModel, OpenAIGPTTokenizer
+from transformers import XLNetLMHeadModel, XLNetTokenizer
+from transformers import TransfoXLLMHeadModel, TransfoXLTokenizer
+
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt = '%m/%d/%Y %H:%M:%S',
@@ -37,13 +41,30 @@ logger = logging.getLogger(__name__)
 
 MAX_LENGTH = int(10000)  # Hardcoded max length to avoid infinite loop
 
-ALL_MODELS = sum((tuple(conf.pretrained_config_archive_map.keys()) for conf in (GPT2Config, OpenAIGPTConfig)), ())
+ALL_MODELS = sum((tuple(conf.pretrained_config_archive_map.keys()) for conf in (GPT2Config, OpenAIGPTConfig, XLNetConfig, TransfoXLConfig)), ())
 
 MODEL_CLASSES = {
     'gpt2': (GPT2LMHeadModel, GPT2Tokenizer),
     'openai-gpt': (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer),
+    'xlnet': (XLNetLMHeadModel, XLNetTokenizer),
+    'transfo-xl': (TransfoXLLMHeadModel, TransfoXLTokenizer),
 }
 
+# Padding text to help Transformer-XL and XLNet with short prompts as proposed by Aman Rusia
+# in https://github.com/rusiaaman/XLNet-gen#methodology
+# and https://medium.com/@amanrusia/xlnet-speaks-comparison-to-gpt-2-ea1a4e9ba39e
+PADDING_TEXT = """ In 1991, the remains of Russian Tsar Nicholas II and his family
+(except for Alexei and Maria) are discovered.
+The voice of Nicholas's young son, Tsarevich Alexei Nikolaevich, narrates the
+remainder of the story. 1883 Western Siberia,
+a young Grigori Rasputin is asked by his father and a group of men to perform magic.
+Rasputin has a vision and denounces one of the men as a horse thief. Although his
+father initially slaps him for making such an accusation, Rasputin watches as the
+man is chased outside and beaten. Twenty years later, Rasputin sees a vision of
+the Virgin Mary, prompting him to become a priest. Rasputin quickly becomes famous,
+with people, even a bishop, begging for his blessing. <eod> </s> <eos>"""
+
+# crashes with no GPU
 def set_seed(args=None, seed=42):
     if args is None:
         np.random.seed(seed)
@@ -87,11 +108,60 @@ def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')
         logits[indices_to_remove] = filter_value
     return logits
 
-def sample_sequence(model, length, context, num_samples=1, temperature=1, top_k=0, top_p=0.0, is_xlnet=False, device='cpu'):
+def get_lookahead_entropies(model, context, batch_size, vocab_size, candidates=None, is_xlnet=False, device='cpu'):
+    # in: context (1-dim tensor of t tokens)
+    # out: ents (1-dim tensor of entropies of w_{t+2} given each candidate w_{t+1})
+    #      i.e. ents[w] = H(w_{t+2} | w_{<=t}, w_{t+1}=w)
+    # tries all words by default, but can pass in array of candidates
+
+    batch = torch.zeros(len(context)+1, dtype=torch.long, device=device)
+    batch[:len(context)] = context
+    batch = batch.unsqueeze(0).repeat(batch_size, 1)
+
+    # mark uncomputed entropies with -1 for processing later
+    ents = -torch.ones(vocab_size, device=device)
+
+    # if not passed an array of candidates, try all candidates
+    if candidates is None:
+        candidates = torch.arange(vocab_size, dtype=torch.long, device=device)
+
+    batch_start = 0
+
+    with torch.no_grad():
+        # loop over all w_{t+1}, chunking into batches
+        while batch_start < len(candidates):
+            batch_end = min(len(candidates), batch_start + batch_size)
+            batch[:batch_end-batch_start, -1] = candidates[batch_start:batch_end]
+
+            inputs = {'input_ids': batch}
+            if is_xlnet: 
+                # XLNet is a direct (predict same token, not next token) and bi-directional model by default
+                # => need one additional dummy token in the input (will be masked), attention mask and target mapping (see model docstring)
+                input_ids = torch.cat((generated, torch.zeros((1, 1), dtype=torch.long, device=device)), dim=1)
+                perm_mask = torch.zeros((1, input_ids.shape[1], input_ids.shape[1]), dtype=torch.float, device=device)
+                perm_mask[:, :, -1] = 1.0  # Previous tokens don't see last token
+                target_mapping = torch.zeros((1, 1, input_ids.shape[1]), dtype=torch.float, device=device)
+                target_mapping[0, 0, -1] = 1.0  # predict last token
+                inputs = {'input_ids': input_ids, 'perm_mask': perm_mask, 'target_mapping': target_mapping}
+
+            outputs = model(**inputs)  # Note: we could also use 'past' with GPT-2/Transfo-XL/XLNet (cached hidden-states)
+            next_token_logits = outputs[0][:, -1, :]
+            
+            next_probs = F.softmax(next_token_logits, dim=-1)
+            next_ents = torch.sum(-next_probs * torch.log(next_probs + 1e-20), dim=-1)
+            
+            ents[batch_start:batch_end] = next_ents[:batch_end-batch_start]
+            batch_start += batch_size
+
+    return ents
+
+def sample_sequence_calibrated(model, length, context, batch_size, vocab_size, alpha=0.0, temperature=1, top_k=0, is_xlnet=False, device='cpu'):
+    num_samples = 1 # since entropy measurement is slow, no point parallelizing
+    
     context = torch.tensor(context, dtype=torch.long, device=device)
     context = context.unsqueeze(0).repeat(num_samples, 1)
     generated = context
-
+    
     # ents[k,t] = H(w_t | w_<t) for independent generation k
     ents = torch.zeros((num_samples, length), device=device)
 
@@ -99,26 +169,46 @@ def sample_sequence(model, length, context, num_samples=1, temperature=1, top_k=
         for gen_index in trange(length):
 
             inputs = {'input_ids': generated}
+            if is_xlnet: 
+                # XLNet is a direct (predict same token, not next token) and bi-directional model by default
+                # => need one additional dummy token in the input (will be masked), attention mask and target mapping (see model docstring)
+                input_ids = torch.cat((generated, torch.zeros((1, 1), dtype=torch.long, device=device)), dim=1)
+                perm_mask = torch.zeros((1, input_ids.shape[1], input_ids.shape[1]), dtype=torch.float, device=device)
+                perm_mask[:, :, -1] = 1.0  # Previous tokens don't see last token
+                target_mapping = torch.zeros((1, 1, input_ids.shape[1]), dtype=torch.float, device=device)
+                target_mapping[0, 0, -1] = 1.0  # predict last token
+                inputs = {'input_ids': input_ids, 'perm_mask': perm_mask, 'target_mapping': target_mapping}
 
             outputs = model(**inputs)  # Note: we could also use 'past' with GPT-2/Transfo-XL/XLNet (cached hidden-states)
-            next_token_logits = outputs[0][:, -1, :] / temperature
-            filtered_logits = next_token_logits # top_k_top_p_filtering(next_token_logits, top_k=top_k, top_p=top_p)
+            model_next_logits = outputs[0][:, -1, :] / temperature
+            
+            if top_k == 0:
+                candidates = None # try all words for lookahead
+            else:
+                # try k most likely words
+                candidates = torch.argsort(model_next_logits[0], descending=True)[:top_k]
 
-            next_probs = F.softmax(filtered_logits, dim=-1)
+            lookahead_ents = get_lookahead_entropies(model, generated[0], batch_size, vocab_size, candidates=candidates, device=device, is_xlnet=is_xlnet).unsqueeze(0)
+            
+            if top_k != 0:
+                # replace uncomputed entropies with average (for centered adjustment)
+                top_average_ent = lookahead_ents[lookahead_ents != -1].mean()
+                lookahead_ents[lookahead_ents != -1] = top_average_ent
+            
+            calibrated_next_logits = model_next_logits + alpha*lookahead_ents 
+
+            next_probs = F.softmax(calibrated_next_logits, dim=-1)
             next_ents = torch.sum(-next_probs * torch.log(next_probs + 1e-20), dim=-1)
-
+            
             ents[:, gen_index] = next_ents
 
-            next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
+            next_token = torch.multinomial(F.softmax(calibrated_next_logits, dim=-1), num_samples=1)
             generated = torch.cat((generated, next_token), dim=1)
     return generated, ents
 
-# TODO: make script work with no GPU
-# currently disregards top_k and top_p. params not used in sample_sequence
-def run(context, model_type = 'gpt2', length=100, temp=1.0, top_k=0, top_p=0.9, num_samples=10, device='cuda', batch=10):
-
+# note: will crash if no cuda
+def run(context, model_type='gpt2', length=100, temp=1.0, top_k=0, top_p=0.0, is_xlnet=False, alpha=0.0, device='cuda', num_samples=10):
     set_seed()
-
     model_class, tokenizer_class = MODEL_CLASSES[model_type]
     tokenizer = tokenizer_class.from_pretrained(model_type)
     model = model_class.from_pretrained(model_type)
@@ -130,15 +220,16 @@ def run(context, model_type = 'gpt2', length=100, temp=1.0, top_k=0, top_p=0.9, 
     avg_ents = torch.zeros((1, length), device=device)
 
     for i in range(num_samples):
-        _, ents = sample_sequence(
+        _, ents = sample_sequence_calibrated(
             model=model,
-            context=context_tokens,
             length=length,
+            context=context_tokens,
+            batch_size=10,
+            vocab_size=tokenizer.vocab_size,
+            alpha=alpha,
             temperature=temp,
             top_k=top_k,
-            top_p=top_p,
             device=device,
-            num_samples=batch,
         )
 
         ents = ents.mean(axis=0)
@@ -151,24 +242,6 @@ def run(context, model_type = 'gpt2', length=100, temp=1.0, top_k=0, top_p=0.9, 
 
     return avg_ents
 
-def get_distr(context, model_type = 'gpt2', temp=1.0, top_k=0, top_p=0.9, num_samples=10, device='cuda'):
-    set_seed()
-
-    model_class, tokenizer_class = MODEL_CLASSES[model_type]
-    tokenizer = tokenizer_class.from_pretrained(model_type)
-    model = model_class.from_pretrained(model_type)
-    model.to(device)
-    model.eval()
-
-    context = torch.tensor(tokenizer.encode(context), dtype=torch.long, device=device)
-    context = context.unsqueeze(0).repeat(num_samples, 1)
-    generated = context
-
-    with torch.no_grad():
-        inputs = {'input_ids': generated}
-        outputs = model(**inputs)
-
-    return outputs
 
 def main():
     parser = argparse.ArgumentParser()
@@ -181,13 +254,12 @@ def main():
     parser.add_argument("--length", type=int, default=20)
     parser.add_argument("--temperature", type=float, default=1.0)
     parser.add_argument("--top_k", type=int, default=0)
-    parser.add_argument("--top_p", type=float, default=0.9)
+    parser.add_argument("--alpha", type=float, default=0)
+    parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--no_cuda", action='store_true',
                         help="Avoid using CUDA when available")
     parser.add_argument('--seed', type=int, default=42,
                         help="random seed for initialization")
-    parser.add_argument('--num_samples', type=int, default=4,
-                        help="number of independent generations")
     args = parser.parse_args()
 
     args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
@@ -205,11 +277,14 @@ def main():
     if args.length < 0 and model.config.max_position_embeddings > 0:
         args.length = model.config.max_position_embeddings
     elif 0 < model.config.max_position_embeddings < args.length:
-        args.length = model.config.max_position_embeddings  # No generation bigger than model size
+        args.length = model.config.max_position_embeddings  # No generation bigger than model size 
     elif args.length < 0:
         args.length = MAX_LENGTH  # avoid infinite loop
 
     print(args)
+
+    vocab_size = tokenizer.vocab_size
+    print('vocab_size:', vocab_size)
 
     while True:
         raw_text = args.prompt if args.prompt else input("Model prompt >>> ")
@@ -217,28 +292,27 @@ def main():
             # Models with memory likes to have a long prompt for short inputs.
             raw_text = (args.padding_text if args.padding_text else PADDING_TEXT) + raw_text
         context_tokens = tokenizer.encode(raw_text)
-        out, ents = sample_sequence(
+        out, ents = sample_sequence_calibrated(
             model=model,
             context=context_tokens,
             length=args.length,
+            batch_size=args.batch_size,
+            vocab_size=vocab_size,
+            alpha=args.alpha,
             temperature=args.temperature,
             top_k=args.top_k,
-            top_p=args.top_p,
             device=args.device,
-            num_samples=args.num_samples,
             is_xlnet=bool(args.model_type == "xlnet"),
         )
-
+        
         # show all generations from this batch
         for i in range(len(out)):
             seq = out[i, len(context_tokens):].tolist()
             text = tokenizer.decode(seq, clean_up_tokenization_spaces=True)
-            print(text)
+            print(text) 
 
-        sve = ents.mean(axis=0)
-        np.savez('test.npz', test=sve)
         print(ents.mean(axis=0)) # average entropies over batch
-
+        
         if args.prompt:
             break
     return text
@@ -246,5 +320,5 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    out, ents = run("To be or not to be, that is the question")
-    print(ents)
+    generated, ents = run("To be or not to be, that is the question:")
+    print(generated, ents)
